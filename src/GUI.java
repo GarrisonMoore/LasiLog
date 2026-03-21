a9fe1a95315d56d130b70e448dea836c93d8caae
GarrisonMoore <mooreg444@gmail.com>
2026-03-21 01:30:51 -0700

    bruh

--- src/GUI.java
+++ src/GUI.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-/// bruh
+/// bruh what
 public class GUI extends JFrame {
     private final DefaultListModel<String> listModel = new DefaultListModel<>();
     private final JList<String> hostList = new JList<>(listModel);
